8d2ff508ab8bca0e8d061e121c3512cc166ea401
peible <abaibaimukan0500@gmail.com>
2024-07-28 15:23:03 +0500

    all

--- scripts/counter.sh
+++ scripts/counter.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+list=$(ls -l | tail -n +2)
+
+list_array=(0 0)
+while IFS= read -r line; do
+    if [[ ${line:0:1} == d ]]; then
+        ((list_array[0]++))
+    elif [[ ${line:0:1} == '-' ]]; then
+        ((list_array[1]++))
+    else
+        echo "err"
+    fi
+done <<< "$list"
+
+echo "Directories: ${list_array[0]}"
+echo "Files: ${list_array[1]}"
--- scripts/password.sh
+++ scripts/password.sh
@@ -2,10 +2,10 @@
 echo "enter a number of lengths"
 read length
 password=""
-arr=(\! \@ \# \$ \% \& \* \_ \- a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+arr=(\! \@ \# \$ \% \& \* \_ \- a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0)
 
 for i in $(seq 1 $length); do
-  password+=${arr[$RANDOM % 61]}
+  password+=${arr[$RANDOM % 21]}
 done
 
 echo $password >> password.txt
